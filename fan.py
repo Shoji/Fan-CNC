63f6dc0eda4db26b2fc20dd407f47e120748f45f
Shoji <michaelmeschede13@gmail.com>
2018-06-03 22:58:03 -0400

    Update fan.py

--- fan.py
+++ fan.py
@@ -10,27 +10,27 @@ def led_on():
 
 def led_off():
 	arduinoData.write('0') # fan is turned off
+def main():
+	print("[Welcome to the Fan CNC] ")
+	print("Do you want to start the fan? ")
+	question = raw_input("(Y)es or (N)o: ") # Question to turn it on
 
-print("[Welcome to the Fan CNC] ")
-print("Do you want to start the fan? ")
-question = raw_input("(Y)es or (N)o: ") # Question to turn it on
+	if question == 'y' \
+	or question == 'Y':
+		led_on() # If answered with y or Y, the fan will turn on
+  		print("Fan has started")
+		time.sleep(5)
+		question2 = raw_input("0 for off, 1 to keep the fan on")
+		if question2 == '1':
+			led_on()
+		elif question2 == '0':
+			led_off()
 
-if question == 'y' or question == 'Y':
-	time.sleep(2)
-	led_on() # If answered with y or Y, the fan will turn on
-  	print("Fan has started")
-	time.sleep(2)
-	question2 = raw_input("Would you like to keep running or turn the fan off, y/n?")
-	time.sleep(1)
-	if question2 == 'y' or question2 == 'Y':
-		led_on()
-		print("The fan is stably running")
-		time.sleep(2)
-	elif question2 == 'N' or question2 == 'n':
+	elif question == 'n' \
+	or question == 'N': # If answered with n or N, the fan will not turn on
 		led_off()
-		time.sleep(2)
-elif question == 'n' or question == 'N': # If answered with n or N, the fan will not turn on
-	led_off()
-  	print("Fan will not start")
-else:
-  print("That is not an option") # Any other answer that is not yes or no is disregarded and nothing happens
+  		print("Fan will not start")
+	else:
+  		print("That is not an option") # Any other answer that is not yes or no is disregarded and nothing happens
+
+main()
